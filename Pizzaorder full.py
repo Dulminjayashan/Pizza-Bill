131b505ec1acde392ed4a6e22ded939807062de4
Dulminjayashan <126898969+Dulminjayashan@users.noreply.github.com>
2023-03-20 17:25:03 +0530

    Add files via upload

---
+++ Pizzaorder full.py
@@ -0,0 +1,162 @@
+while True:
+    print("\n")
+    print("        Welcome to ABC Pizza         ")
+    print("\n")
+    print("Menu ")
+    print("1.Vegi Pizza - Rs.400.00")
+    print("2.Seafood Pizza - Rs.800.00")
+    print("3.chicken Pizza - Rs.600.00")
+    total=0
+    total2=0
+    total3=0
+    total4=0
+    total5=0
+    
+    print("\n")
+    #MENU
+    while True:
+        choice = input("Enter your choice (1, 2, or 3): ")
+        if choice == '1':
+            pizza = "Vegi Pizza"
+            total= total+400.00
+            break
+        elif choice == '2':
+            pizza = "Seafood Pizza"
+            total= total+800.00      
+            break
+        elif choice == '3':
+            pizza = "Chicken Pizza"
+            total= total+600.00      
+            break
+        else:
+            print("Invalid choice. Please try again.")
+            
+    #EXTRA ADDING
+    print("Do you want something extra added?")
+    extra = str(input("Enter YES or NO: ").upper())
+    print("\n")
+    if extra == "YES":
+        print("1. Extra cheese - Rs.100.00")
+        print("2. Extra onions - Rs.40.00")
+        print("3. Extra sauce - Rs.60.00")
+       
+        while True:
+            extra = input("Enter your choice (1, 2, or 3): ")
+            if extra == '1':
+                extra = "Extra cheese"
+                total2= total2+100.00
+                break
+            elif extra == '2':
+                extra = "Extra onions"
+                total2= total2+40.00
+                break
+            elif extra == '3':
+                extra = "Extra sauce"
+                total2= total2+60.00
+                break
+            else:
+                print("Invalid choice. Please try again.")
+                        
+        print("Do you want more extra added?")
+        extra1 = str(input("Enter YES or NO: ").upper())
+        if extra1 == "YES":   
+            while True:
+                extra1 = input("Enter your choice (another number): ")
+                if extra1 == '1':
+                    extra1 = "Extra cheese"
+                    total3= total3+100.00
+                    break
+                elif extra1 == '2':
+                    extra1 = "Extra onions"
+                    total3= total3+40.00
+                    break
+                elif extra1 == '3':
+                    extra1 = "Extra sauce"
+                    total3= total3+60.00
+                    break
+                else:
+                    print("Invalid choice. Please try again.")
+
+              
+                        
+            print("Do you want more extra added?")
+            extra2 = str(input("Enter YES or NO: ").upper())
+            if extra2 == "YES":   
+                while True:
+                    extra2 = input("Enter your choice (another number): ")
+                    if extra2 == '1':
+                        extra2 = "Extra cheese"
+                        total4= total4+100.00
+                        break
+                    elif extra2 == '2':
+                        extra2 = "Extra onions"
+                        total4= total4+40.00
+                        break
+                    elif extra2 == '3':
+                        extra2 = "Extra sauce"
+                        total4= total4+60.00
+                        break
+                    else:
+                        print("Invalid choice. Please try again.")
+                        
+            else:
+                total4= 0.00
+                print("No extras added.")
+
+
+        else:
+            total3= 0.00
+            print("No extras added.")
+                        
+                        
+    else:
+        total2= 0.00
+        print("No extras added.")
+        
+    print("\n")                                  
+    #DRINKS
+    print("Do you want some drinks?")
+    drink = str(input("Enter YES or NO: ").upper())
+    print("\n") 
+    if drink == "YES":    
+        print("What drink would you like?")
+        print("1. Coke - Rs.200.00")
+        print("2. Pepsi - Rs.200.00")
+        print("3. Water - Rs.100.00")
+
+        while True:
+            drink = input("Enter your choice (1, 2, or 3): ")
+            if drink == '1':
+                drink = "Coke"
+                total5= total5+200.00
+                break
+            elif drink == '2':
+                drink = "Pepsi"
+                total5= total5+200.00
+                break
+            elif drink == '3':
+                drink = "Water"
+                total5= total5+100.00
+                break
+            else:
+                print("Invalid choice. Please try again.")
+    else:
+         total5= 0.00
+         print("No Drinks.")
+    #TP
+    tp=total+total2+total3+total4+total5
+    print("\n")
+    
+    #BILL
+    print(f"Your order: {pizza}")
+    print(f"Added: {extra}")
+    print(f"Added 2: {extra1}")
+    print(f"Added 3: {extra2}")
+    print(f"Drinks: {drink}")
+    print("Total Price: ",str("Rs"),(tp))
+    
+    print("\n")
+    while True:
+        a = input("Next Customer (Press Enter) ")
+        break
+        
